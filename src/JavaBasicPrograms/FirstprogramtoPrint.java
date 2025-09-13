efb5b932f8202f685fd595cf2710ea34cb9a6c1a
Chirag Shah <ChiragShah.world@gmail.com>
2025-09-13 10:04:58 +0530

    Updated the basic of programming

--- src/JavaBasicPrograms/FirstprogramtoPrint.java
+++ src/JavaBasicPrograms/FirstprogramtoPrint.java
@@ -5,5 +5,11 @@ public class FirstprogramtoPrint
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming");
+        System.out.print("First check");
+        System.out.println("SingleLine  -> //,println new line in the console");
+        System.out.println("MultiLine  -> /* */");
+        System.out.println("Shortcut:\nComment / uncomment with line comment Ctrl + /.  ( cmd+/)");
+        System.out.print("Comment / uncomment with block comment Ctrl + Shift + /");
     }
 }
